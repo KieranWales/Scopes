7b2bc0314ca3b4a65b3da245f885931ee32b23d3
Kieran Wales <17WalesK@fulford.york.sch.uk>
2022-09-21 12:02:54 +0100

    Add project files.

---
+++ Scopes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scopes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello("");
+        }
+
+        private static void SayHello(string language)
+        {
+            switch (language.ToLower())
+            {
+                case "english":
+                    Console.WriteLine("Hello");
+                    break;
+
+                case "german":
+                    Console.WriteLine("Hallo");
+                    break;
+
+                case "french":
+                    Console.WriteLine("Bonjour");
+                    break;
+
+                case "spanish":
+                    Console.WriteLine("Hola");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
